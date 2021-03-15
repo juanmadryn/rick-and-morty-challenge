48b71b0380e6bee447b1cca28a15e22a719995b3
Juan Manuel Cortez <jcortez@sursoftware.com.ar>
2021-03-15 15:23:30 -0300

    feat(backend): :sparkles: Koa static middleware
    It was added de koa-static module to serve the frontend app

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,7 @@
 import Koa from "koa";
 import bodyParser from "koa-bodyparser";
+import { default as koaStatic } from "koa-static";
+import { default as send } from "koa-send";
 import winston from "winston";
 
 import { logger } from "./logger";
@@ -14,9 +16,25 @@ app.use(logger(winston));
 // Enable bodyParser with default options
 app.use(bodyParser());
 
+console.log(__dirname);
+app.use(koaStatic(`${__dirname}/../public`));
+
 // These routes are protected by the JWT middleware, also include middleware to respond with "Method Not Allowed - 405".
 app.use(routes.routes()).use(routes.allowedMethods());
 
 app.listen(config.port);
+
+// this last middleware catches any request that isn't handled by
+// koa-static or koa-router, ie your index.html in your example
+app.use(async function (ctx, next) {
+    return send(
+        ctx, 
+        '/index.html', 
+        { 
+            root: `${__dirname}/../public`
+        }
+    )
+    .then(() => next())
+});
 
 console.log(`Server running on port ${config.port}`);
